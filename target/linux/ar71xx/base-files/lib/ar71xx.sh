TobleMiner/source

ar71xx: Add support for Ubiquiti NanoStation AC loco
Atheros AR9342, 16 MB flash, 64 MB RAM

Successor to the old NanoStation M5 loco with AC wireless.

Includes a mac80211 patch for ath10k_pci because Ubiquiti uses a Ubiquiti
branded and customized QCA988X with vendor id 0777 and device id 11ac for
AC wireless.

Installation

1. Connect to serial header on device (8N1 115200)
2. Power on device and enter uboot console
3. Set up tftp server serving an openwrt initramfs build
4. Load initramfs build using the command tftpboot in the uboot cli
5. Boot the loaded image using the command bootm
6. Copy squashfs openwrt sysupgrade build to the booted device
7. Use mtd to write sysupgrade to partition "firmware"
8. Reboot and enjoy

Signed-off-by: Tobias Schramm <tobleminer@gmail.com>

target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -1259,6 +1259,9 @@ ar71xx_board_detect() {
 	*"UniFi-AC-LITE/MESH")
 		name="unifiac-lite"
 		;;
+	*"Ubiquiti Nanostation AC loco")
+		name="nanostationac"
+		;;
 	*"UniFi-AC-PRO")
 		name="unifiac-pro"
 		;;

TobleMiner/openwrt

target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -1337,6 +1337,9 @@ ar71xx_board_detect() {
 	*"Tube2H")
 		name="tube2h"
 		;;
+	*"Ubiquiti Nanostation AC loco")
+		name="nanostationacl"
+		;;
 	*"UniFi")
 		name="unifi"
 		;;
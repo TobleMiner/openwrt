633b06f6a17f9fa19185a8fa4b292045c21ef4df
Tobias Schramm <tobleminer@gmail.com>
2018-06-26 21:13:09 +0200

    ar71xx: Fix switch init on Ubiquiti Nanostation AC

--- target/linux/ar71xx/files/arch/mips/ath79/mach-ubnt-wa.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-ubnt-wa.c
@@ -49,6 +49,33 @@ static struct gpio_keys_button ubnt_wa_gpio_keys[] __initdata = {
 	}
 };
 
+static struct ar8327_pad_cfg nanostation_ac_ar8327_pad0_cfg = {
+	.mode = AR8327_PAD_MAC_RGMII,
+	.txclk_delay_en = true,
+	.rxclk_delay_en = true,
+	.txclk_delay_sel = AR8327_CLK_DELAY_SEL1,
+	.rxclk_delay_sel = AR8327_CLK_DELAY_SEL2,
+};
+
+static struct ar8327_platform_data nanostation_ac_ar8327_data = {
+	.pad0_cfg = &nanostation_ac_ar8327_pad0_cfg,
+	.port0_cfg = {
+		.force_link = 1,
+		.speed = AR8327_PORT_SPEED_1000,
+		.duplex = 1,
+		.txpause = 1,
+		.rxpause = 1,
+	},
+};
+
+static struct mdio_board_info nanostation_ac_mdio0_info[] = {
+	{
+		.bus_id = "ag71xx-mdio.0",
+		.phy_addr = 0,
+		.platform_data = &nanostation_ac_ar8327_data,
+	},
+};
+
 static void __init ubnt_wa_setup(void)
 {
 	u8 *eeprom = (u8 *) KSEG1ADDR(0x1fff0000);
@@ -98,16 +125,41 @@ MIPS_MACHINE(ATH79_MACH_UBNT_LITEBEAMACAP, "UBNT-LITEBEAMACAP",
 MIPS_MACHINE(ATH79_MACH_UBNT_NANOSTATIONACL, "UBNT-NANOSTATION-ACL",
 	     "Ubiquiti Nanostation AC loco", ubnt_wa_setup);
 
-
 static void __init ubnt_nanostationac_setup(void)
 {
-	ubnt_wa_setup();
+	u8 *eeprom = (u8 *) KSEG1ADDR(0x1fff0000);
 
-//	ath79_eth1_data.phy_if_mode = PHY_INTERFACE_MODE_RGMII;
-//	ath79_eth1_data.mii_bus_dev = &ath79_mdio0_device.dev;
-//	ath79_eth1_data.phy_mask = BIT(UBNT_WA_PHY_ID);
-//	ath79_eth1_pll_data.pll_1000 = 0x06000000;
-//	ath79_eth1_pll_data.pll_10 = 0x00001313;
+	ath79_register_m25p80(&ubnt_wa_flash_data);
+
+	// Register mdio interface
+	ath79_register_mdio(0, 0x00);
+	mdiobus_register_board_info(nanostation_ac_mdio0_info,
+				    ARRAY_SIZE(nanostation_ac_mdio0_info));
+
+	ath79_setup_ar934x_eth_cfg(AR934X_ETH_CFG_RGMII_GMAC0);
+
+	ath79_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_RGMII;
+	ath79_eth0_data.mii_bus_dev = &ath79_mdio0_device.dev;
+	ath79_eth0_data.phy_mask = BIT(0);
+	ath79_eth0_data.speed = SPEED_1000;
+	ath79_eth0_data.duplex = DUPLEX_FULL;
+	ath79_eth0_pll_data.pll_1000 = 0x06000000;
+
+
+	ath79_init_mac(ath79_eth0_data.mac_addr, eeprom
+		+ UBNT_WA_ETH0_MAC_OFFSET, 0);
+	ath79_register_eth(0);
+
+
+	ath79_register_wmac(eeprom
+		+ UBNT_WA_WMAC_CALDATA_OFFSET, NULL);
+
+
+	ap91_pci_init(eeprom + UBNT_WA_PCI_CALDATA_OFFSET, NULL);
+
+	ath79_register_gpio_keys_polled(-1, UBNT_WA_KEYS_POLL_INTERVAL,
+	                                ARRAY_SIZE(ubnt_wa_gpio_keys),
+	                                ubnt_wa_gpio_keys);
 }
 
 MIPS_MACHINE(ATH79_MACH_UBNT_NANOSTATIONAC, "UBNT-NANOSTATION-AC",
